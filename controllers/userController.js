3d61fbce4ef437cb487ffaf58b3c29a6bcd482e1
Riswan N <91376359+risudev@users.noreply.github.com>
2025-09-27 12:51:01 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -123,8 +123,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     res.status(200).cookie("token", "", {
         expires: new Date(Date.now()),
         httpOnly: true,
-        // secure: true,
-        // sameSite: "None"
+        secure: true,
+        sameSite: "None"
     })
         .json({
             success: true,
